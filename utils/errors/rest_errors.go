5f244e255ed7389cc2776f779829a75a5636722c
ibrahim-akrab <ibrahim.m.akrab@gmail.com>
2021-05-28 18:41:26 +0200

    add basic rest errors

--- utils/errors/rest_errors.go
+++ utils/errors/rest_errors.go
@@ -1,7 +1,33 @@
 package errors
 
+import "net/http"
+
 type RestErr struct {
 	Message string `json:"message"`
 	Status  int    `json:"status"`
 	Error   error  `json:"error"`
+}
+
+func NewInternalServerError(err error) *RestErr {
+	return &RestErr{
+		Message: http.StatusText(http.StatusInternalServerError),
+		Status:  http.StatusInternalServerError,
+		Error:   err,
+	}
+}
+
+func NewBadRequestError(err error) *RestErr {
+	return &RestErr{
+		Message: http.StatusText(http.StatusBadRequest),
+		Status:  http.StatusBadRequest,
+		Error:   err,
+	}
+}
+
+func NewNotFoundError(err error) *RestErr {
+	return &RestErr{
+		Message: http.StatusText(http.StatusNotFound),
+		Status:  http.StatusNotFound,
+		Error:   err,
+	}
 }
